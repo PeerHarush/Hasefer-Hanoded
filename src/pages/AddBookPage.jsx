8c440a9afc36f94fcddfeb5570b8e7df557921f5
Ofir172 <ofir170298@gmail.com>
2025-04-27 18:09:53 +0300

    WIP: שינויי עבודה זמניים

--- src/pages/AddBookPage.jsx
+++ src/pages/AddBookPage.jsx
@@ -1,16 +1,48 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Wrapper } from '../styles/History.styles';
-import { Table } from 'react-bootstrap';
+import { Wrapper, Card, Title, Subtitle, FormGroup, Label, Input, Button } from '../styles/AddBookPage.styles';
 
-function AddBook() {
+const AddBookPage = () => {
   return (
+    <Wrapper>
+      <Card>
+        <Title>הוסף ספר</Title>
+        <Subtitle>הזן את פרטי הספר שברצונך להוסיף</Subtitle>
+        <form>
+          <FormGroup>
+            <Label htmlFor="bookTitle">שם הספר</Label>
+            <Input type="text" id="bookTitle" placeholder="הכנס את שם הספר" />
+          </FormGroup>
+          
+          <FormGroup>
+            <Label htmlFor="bookAuthor">מחבר</Label>
+            <Input type="text" id="bookAuthor" placeholder="הכנס שם מחבר" />
+          </FormGroup>
 
-    
+          <FormGroup>
+            <Label htmlFor="bookGenre">ז'אנר</Label>
+            <Input type="text" id="bookGenre" placeholder="הכנס ז'אנר" />
+          </FormGroup>
+
+          <FormGroup>
+            <Label htmlFor="bookDescription">תקציר הספר</Label>
+            <Input type="text" id="bookDescription" placeholder="הכנס תקציר" />
+          </FormGroup>
+
+          <FormGroup>
+            <Label htmlFor="bookPrice">מחיר </Label>
+            <Input type="number" id="bookPrice" placeholder="הכנס מחיר" />
+          </FormGroup>
+
+          <FormGroup>
+            <Label htmlFor="bookPlace">מיקום </Label>
+            <Input type="text" id="bookPlace" placeholder="הכנס מיקום" />
+          </FormGroup>
+
+          <Button type="submit">הוסף ספר</Button>
+        </form>
+      </Card>
+    </Wrapper>
   );
-}
+};
 
-
-
-
-export default AddBook;
+export default AddBookPage;
